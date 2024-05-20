03b0d073249f3ca95e99bae3aa4578818dc4fe14
Edgar Aldana <eddaldana95@gmail.com>
2024-05-19 20:26:07 -0600

    ✨feat: Add Module Signup

--- src/logic/services/auth/login.ts
+++ src/logic/services/auth/login.ts
@@ -11,11 +11,42 @@ type SendLoginPayload = {
     token_type: string;
 }
 
+
+type SendSignupIN = {
+    
+    nombres: string, 
+    apellidoPaterno: string,
+    apellidoMaterno: string | undefined
+    correo: string
+    password: string
+    posicion: string
+    matriculaVehiculo: string
+    region: string
+    telefono: string
+
+}
+
+type SendSignupPayload = {
+    username: string
+}
+
 //enviar login 
 export async function sendLogin(dataToSend: SendLoginIN) {
     const authToken = "No needed";
     const path = "/auth/login";
     const response = await _$post<SendLoginPayload, SendLoginIN>(
+        path, dataToSend, authToken
+    );
+    return response;
+}
+
+
+
+export async function sendSignUp(dataToSend: SendSignupIN) {
+    
+    const authToken = "No needed";
+    const path = "/auth/signup";
+    const response = await _$post<SendSignupPayload, SendSignupIN>(
         path, dataToSend, authToken
     );
     return response;
--- src/ui/signup/molecules/SignupForm/SignupForm.tsx
+++ src/ui/signup/molecules/SignupForm/SignupForm.tsx
@@ -3,63 +3,231 @@ import { useState } from "react";
 import styles from "./SignupForm.styles.module.scss";
 import Input from "../../../sharing/atoms/Input/Input";
 import PasswordInput from "../../../sharing/atoms/PasswordInput/PasswordInput";
+import Button from "../../../sharing/atoms/Button/Button"
+import { InputStates, SelectStates } from "@types";
+import Select from "../../../sharing/atoms/Select/Select";
+import IconFactory from "../../../sharing/atoms/IconFactory/IconFactory";
+import { sendSignUp } from "@services/auth/login";
+import { validateInputState } from "@utils/validateInputs";
+import Loader from "../../../sharing/atoms/Loader/Loader";
 
 
 export default function SignupForm(){
+
+    const [isLoader, setIsLoader] = useState(false);
+    const [errorText, setErrorText] = useState("");
+    const [formStates, setFormStates] = useState({
+     
+        name: { value: "", mood: "default", helperText: "" } as InputStates,
+        lastname: { value: "", mood: "default", helperText: "" } as InputStates,
+        lastnameTwo: { value: "", mood: "default", helperText: "" } as InputStates,
+        email: { value: "", mood: "default", helperText: "" } as InputStates,
+        password: { value: "", mood: "default", helperText: "" } as InputStates,
+        workPosition: {value: "Puesto de trabajo", mood: "default", helperText: "" } as SelectStates,
+        phoneNumber: { value: "", mood: "default", helperText: "" } as InputStates,
+        area: { value: "Región", mood: "default", helperText: "" } as SelectStates,
+        dataCar: { value: "-", mood: "filled", helperText: ""} as InputStates
+
+    })
+
+    const handleSignup = () => { 
+        if(validateFields()) 
+            sendSignupToServer(); 
+    }
+
+
+    const validateFields = () : boolean =>  {
+        let newStates = {} as typeof formStates;
+        let retorno = true;
+        for(const campo in formStates){
+            const campo_ = campo as keyof typeof formStates
+            if(campo_ === "email") newStates[campo_] = validateInputState(formStates[campo_], "email");
+            else newStates[campo_] = validateInputState(formStates[campo_], "text");
+            if(newStates[campo_].mood !== "filled") retorno = false;
+        }
+        setFormStates(newStates);
+        return retorno;
+    }
+
+    const onFormChange = (states: InputStates, _: number, identifier: string) => {
+        setFormStates({
+            ...formStates, 
+            [identifier]: states,
+        })
+    }
+
+    const sendSignupToServer = async () => {
+
+        setErrorText("");
+        setIsLoader(true);
+        sendSignUp({
+
+            nombres: formStates.email.value,
+            apellidoPaterno:formStates.lastname.value,
+            apellidoMaterno:formStates.lastnameTwo.value,
+            correo: formStates.email.value,
+            password:formStates.password.value,
+
+            posicion: formStates.workPosition.value,
+            region:formStates.area.value,
+            matriculaVehiculo: formStates.dataCar.value,
+            telefono:formStates.phoneNumber.value
+        })
+        .then(response=>{
+            setIsLoader(false);
+            if('error' in response){
+                alert(`Network error. detail: ${response.error}`);
+            }else if(response.data.success === false || !response.data.success){
+                alert("Usuario registrado en base de datos. Intente recuperar contraseña");
+                window.location.href = "/"; 
+            }else {
+                window.location.href = "/"; 
+            }
+        })
+
+    }
 
     return(
         
         
         <div className={styles.container}>
             
-            <div className={styles.form}>
+            { isLoader && <Loader/> }
+            <div className={styles.SignupForm}>
 
-                <span className={styles.form_title}>Crear una cuenta</span>
+                <div className={styles.form_title}>
 
-                <div className={styles.coolinput}>
-                    <label htmlFor="input" className={styles.text}>Nombre(s)</label>
-                    <input type="text" placeholder="..." name="input" className={styles.input}></input>
+                     <span>Crear una cuenta</span>
+
                 </div>
+                
+                <div className={styles.form}>
 
 
-                <div className={styles.coolinput}>
-                    <label htmlFor="input" className={styles.text}>Apellido Paterno</label>
-                    <input type="text" placeholder="..." name="input" className={styles.input}></input>
-                </div>
+                    <Input 
+                        id={0}
+                        placeholder="Nombres" 
+                        topText="Nombre(s)"
+                        identifier="name"
+                        type="text"
+                        states={formStates.name}
+                        dispatch={onFormChange}
 
-                <div className={styles.coolinput}>
-                    <label htmlFor="input" className={styles.text}>Apellido Materno</label>
-                    <input type="text" placeholder="..." name="input" className={styles.input}></input>
-                </div>
+                    />
 
-                <div className={styles.coolinput}>
-                    <label htmlFor="input" className={styles.text}>Correo electrónico</label>
-                    <input type="text" placeholder="correo@dominio.com" name="input" className={styles.input}></input>
-                </div>
+                    <Input 
+                        id={1}
+                        placeholder="Apellido Paterno" 
+                        topText="Apellido Paterno"
+                        identifier="lastname"
+                        type="text"
+                        states={formStates.lastname}
+                        dispatch={onFormChange}
 
-                <div className={styles.coolinput}>
-                    <label htmlFor="input" className={styles.text}>Password</label>
-                    <input type="password" placeholder="********" name="input" className={styles.input}></input>
-                </div>
+                    />
 
 
-                <div className={styles.coolinput}>
-                    <label htmlFor="input" className={styles.text}>Región</label>
-                    <input type="text" placeholder="Norte, Sur, Este, Oeste" name="input" className={styles.input}></input>
-                </div>
-            
+                    <Input 
+                        id={2}
+                        placeholder="Apellido Materno" 
+                        topText="Apellido Materno"
+                        identifier="lastnameTwo"
+                        type="text"
+                        states={formStates.lastnameTwo}
+                        dispatch={onFormChange}
+
+                    />
+
+
+
+                    <Input 
+                        id={3}
+                        placeholder="Correo electrónico" 
+                        topText="Correo electrónico"
+                        identifier="email"
+                        type="text"
+                        states={formStates.email}
+                        dispatch={onFormChange}
+
+                    />
+                    
+
+                    <Input 
+                        id={4}
+                        placeholder="Contraseña" 
+                        topText="Contraseña"
+                        identifier="password"
+                        type="password"
+                        states={formStates.password}
+                        dispatch={onFormChange}
+
+                    />
+
+
+                    <Select 
+
+                            id={5}
+                            elements={[{id: 1, descripcion: "Monitorista"}, {id: 2, descripcion: "Conductor"}]}
+                            states={formStates.workPosition}
+                            topText="Puesto de trabajo"                       
+                            identifier="workPosition"
+                            dispatch={onFormChange}
+                    />
+
+
+                    <Select 
+
+                        id={6}
+                        elements={[{id: 1, descripcion: "Norte"}, {id: 2, descripcion: "Sur"}, {id: 3, descripcion: "Este"}, {id: 4, descripcion: "Oeste"} ]}
+                        states={formStates.area}
+                        topText="Región"                       
+                        identifier="area"
+                        dispatch={onFormChange}
+
+                    />
+
         
-                <button className={styles.button}>
-                    Registrar
-                    <svg fill="currentColor" viewBox="0 0 24 24" className={styles.icon}>
-                        <path
-                        clip-rule="evenodd"
-                        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-                        fill-rule="evenodd"
-                        ></path>
-                    </svg>
-                </button>
+                    <Input 
 
+                        id={7}
+                        placeholder="Matricula Vehiculo" 
+                        topText="Matricula Vehiculo"
+                        identifier="dataCar"
+                        type="text"
+                        states={formStates.dataCar}
+                        dispatch={onFormChange}
+
+                    />
+
+                    <Input 
+
+                        id={8}
+                        placeholder="Teléfono" 
+                        topText="Teléfono"
+                        identifier="phoneNumber"
+                        type="text"
+                        states={formStates.phoneNumber}
+                        dispatch={onFormChange}
+
+                    />
+
+
+                </div>
+
+
+                <div className={styles.buttonContainer}>
+
+                    <Button 
+                            onClick={handleSignup}
+                            text="Registrar" 
+                            iconRight="arrow_right" 
+                            buttonType="principal" 
+                    />
+
+                </div>
+                
+        
+                
 
             </div>
         </div>
@@ -68,3 +236,11 @@ export default function SignupForm(){
 
     );
 }
+
+function setErrorText(arg0: string) {
+    throw new Error("Function not implemented.");
+}
+function setIsLoader(arg0: boolean) {
+    throw new Error("Function not implemented.");
+}
+
